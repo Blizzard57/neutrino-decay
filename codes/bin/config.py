d0b1d3f63183277cf047283a5dcac623302ef506
Blizzard57 <kalp77000@gmail.com>
2022-03-29 11:15:18 +0530

    Ignoring Config Files

--- codes/bin/config.py
+++
@@ -1,7 +0,0 @@
-# Constants
-HOME_DIR = '/home/blizzard/Research/'
-CODE_DIR = 'codes/bin/'
-DELPHES_FILE = '/Events/run_01/tag_1_delphes_events.root'
-MADGRAPH_DIR = '/home/blizzard/Research/tools/madGraph/'
-DATASET_DIR = 'datasets/csvdata/'
-TXT_DIR = '/home/blizzard/Research/scratch/'
